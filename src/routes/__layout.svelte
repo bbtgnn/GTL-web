9d6ef40505e698bc696bf551a2600d5cf1a5ba78
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-05-13 08:53:40 +0200

    added layout with navbar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,18 @@
 <script>
 	import '../app.css';
+
+	const links = [
+		{ href: '/letters', text: 'Lettere' },
+		{ href: '/syntax', text: 'Sintassi' }
+	];
 </script>
 
+<div class="flex flex-row flex-nowrap p-2 bg-slate-900 gap-2">
+	{#each links as l}
+		<a
+			class="font-mono text-white px-3 py-1 bg-slate-700 hover:bg-slate-600"
+			href={l.href}>{l.text}</a
+		>
+	{/each}
+</div>
 <slot />
