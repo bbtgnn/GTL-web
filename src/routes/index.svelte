3e94a5355fbe94a7865880100275035849792daf
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-05-22 14:28:24 +0200

    added redirection

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts" context="module">
+	/** @type {import('@sveltejs/kit').Load} */
+	export async function load() {
+		return {
+			status: 302,
+			redirect: `/letters`
+		};
+	}
+</script>
