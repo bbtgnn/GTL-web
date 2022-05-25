eaae7dfedc2eedbd57ee64ef259065d910ca02a4
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-05-25 13:45:59 +0200

    rebuild

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -17,13 +19,17 @@ const config = {
 			precompress: false
 		}),
 
+		// paths: {
+		// 	base: dev ? '' : '/GTL-web'
+		// },
+
 		prerender: {
 			// This can be false if you're using a fallback (i.e. SPA mode)
 			default: true
-		}
+		},
 
 		// Needed for github pages
-		// trailingSlash: 'always'
+		trailingSlash: 'always'
 	}
 };
 
