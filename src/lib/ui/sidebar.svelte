ac25dddc2da99b33aa526a61abe41d37b8275189
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-05-26 03:32:00 +0200

    added base sidebar components

---
+++ src/lib/ui/sidebar.svelte
@@ -0,0 +1,40 @@
+<script lang="ts" context="module">
+	import { setContext, getContext } from 'svelte';
+
+	export type Ctx = {
+		selected: string;
+	};
+
+	export const key = {};
+
+	export function setCtx(ctx: Ctx) {
+		setContext<Ctx>(key, ctx);
+	}
+
+	export function getCtx(): Ctx {
+		return getContext(key);
+	}
+</script>
+
+<script lang="ts">
+	export let selected: string = '';
+</script>
+
+<!--  -->
+
+<!-- sidebar -->
+<div class="space-y-6 flex w-60 flex-col flex-nowrap p-6 bg-slate-100">
+	<!-- Slot 1 -->
+	<slot name="topArea" />
+
+	{#if $$slots.topArea}
+		<hr class="border-b-1 border-slate-300" />
+	{/if}
+
+	<!-- Glyph list -->
+	<!-- Slot 2 -->
+	<p class="text-small font-mono text-slate-900 mb-4 text-sm">
+		<slot name="listTitle" />
+	</p>
+	<div class="flex-grow scroll-auto space-y-1" />
+</div>
---
+++ src/lib/ui/sidebarTile.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import Sidebar, { getCtx } from './sidebar.svelte';
+
+	console.log(getCtx());
+
+	export let id: string;
+
+	function select() {}
+</script>
+
+<!-- <div
+	class:text-white={selected}
+	class:bg-slate-800={selected}
+	class:text-slate-900={!selected}
+	class:bg-slate-300={!selected}
+	class="px-3 py-1 {!selected ? 'hover:bg-slate-400' : ''}"
+	on:click={() => {
+		selectGlyph(g);
+	}}
+>
+	{g.name}
+</div> -->
