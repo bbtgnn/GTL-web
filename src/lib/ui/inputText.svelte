56db1af83b4f9508e91044db20b7416df6403cb3
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-05-05 20:46:47 +0200

    feat(InputText): now with auto resize

--- src/lib/ui/inputText.svelte
+++ src/lib/ui/inputText.svelte
@@ -1,8 +1,31 @@
 <script lang="ts">
 	import { nanoid } from 'nanoid';
+	import { onMount } from 'svelte';
 
 	export let value: string;
 	export let name: string = nanoid(5);
+	export let grow = false;
+
+	let thisInput: HTMLInputElement;
+
+	onMount(() => {
+		if (!grow) return;
+		if (!thisInput) return;
+		const parent = thisInput.parentElement;
+		if (!parent) return;
+
+		const resizerObserver = new ResizeObserver((entries) => {
+			const entry = entries[0];
+			if (!entry) return;
+			const { width } = entry.contentRect;
+			thisInput.style.width = `${width}px`;
+		});
+
+		resizerObserver.observe(parent);
+		return () => {
+			resizerObserver.disconnect();
+		};
+	});
 </script>
 
 <!--  -->
@@ -10,6 +33,7 @@
 <input
 	type="text"
 	bind:value
+	bind:this={thisInput}
 	class="p-2 bg-slate-200 font-mono hover:bg-slate-300 focus:ring-4"
 	id={name}
 	{name}
