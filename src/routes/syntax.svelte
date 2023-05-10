acc93f31d02bd326938acd92a071028c270c519b
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-05-10 17:12:26 +0200

    fix(Syntax): updated reactivity

--- src/routes/syntax.svelte
+++ src/routes/syntax.svelte
@@ -120,12 +120,15 @@
 
 	<!-- syntax editor -->
 	<div class="p-8 space-y-8 overflow-y-auto">
-		{#if currentSyntax && currentSyntaxIndex !== undefined}
+		{#if currentSyntaxIndex !== undefined}
 			<div class="flex flex-col mb-8">
 				<p class="text-small font-mono text-slate-900 mb-2 text-sm">
 					Nome stile
 				</p>
-				<InputText name="styleName" bind:value={currentSyntax.name} />
+				<InputText
+					name="styleName"
+					bind:value={$syntaxes[currentSyntaxIndex].name}
+				/>
 			</div>
 			<hr />
 			<SyntaxEditor bind:syntax={$syntaxes[currentSyntaxIndex]} />
@@ -133,8 +136,8 @@
 	</div>
 
 	<div class="p-8 border border-l-gray-300 overflow-y-scroll">
-		{#if currentSyntax}
-			<SyntaxPreview syntax={currentSyntax} />
+		{#if currentSyntaxIndex !== undefined}
+			<SyntaxPreview syntax={$syntaxes[currentSyntaxIndex]} />
 		{/if}
 	</div>
 </div>
