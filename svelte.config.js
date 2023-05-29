5ba6a1577d34124d6fac6b8943746d1299a45cdf
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-05-30 00:04:04 +0200

    setup: tailwind + gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,37 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		// /docs because of github pages
+		adapter: adapter({
+			// default options are shown
+			pages: 'docs',
+			assets: 'docs',
+			fallback: null,
+			precompress: false
+		}),
+
+		paths: {
+			base: dev ? '' : '/GTL-web'
+			// assets: dev ? '' : '/GTL-web/docs'
+		},
+
+		// Needed for github pages
+		trailingSlash: 'always'
 	}
 };
 
